4883a21cb56eb8bc0c26a1428d591b7ae6429008
Smith Rouniyar <130716901+smith8908@users.noreply.github.com>
2025-04-25 15:23:07 +0545

    Update first.java

--- programming in java/first.java
+++ programming in java/first.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
 
+public class first
+  {
+public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the String : ");
+      String a = sc.nextLine();
+      System.out.println("Your name is : " +a);
+    }
+  }
